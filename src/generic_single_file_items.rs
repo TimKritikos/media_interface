66af5e1c9da9d9d03e69f9a197d0ad99fd90ad40
(Tim) Efthimis Kritikos <git-27@mail.efthimiskritikos.com>
2025-11-20 05:49:26 +0000

    Generic-Single-File-Items: Minor cleanup and added 3gpp and png extensions

--- src/generic_single_file_items.rs
+++ src/generic_single_file_items.rs
@@ -7,10 +7,12 @@ use crate::FileItem;
 pub struct GenericSingleFileItem;
 
 fn filetype(ext: &str) -> Result<crate::helpers::JsonFileInfoTypes<'_>> {
-    match ext {
-        "JPG"|"jpg" => Ok(JsonFileInfoTypes{ file_type:"image",item_type:"image" }),
-        "MP4"|"mp4" => Ok(JsonFileInfoTypes{ file_type:"video",item_type:"video" }),
-        "WAV"|"wav" => Ok(JsonFileInfoTypes{ file_type:"audio",item_type:"audio" }),
+    match ext.to_lowercase().as_str() {
+        "jpg"   => Ok(JsonFileInfoTypes{ file_type:"image",item_type:"image" }),
+        "png"   => Ok(JsonFileInfoTypes{ file_type:"image",item_type:"image" }),
+        "mp4"   => Ok(JsonFileInfoTypes{ file_type:"video",item_type:"video" }),
+        "wav"   => Ok(JsonFileInfoTypes{ file_type:"audio",item_type:"audio" }),
+        "3gpp"  => Ok(JsonFileInfoTypes{ file_type:"audio",item_type:"audio" }),
         _ => Err(anyhow::anyhow!("unkown file extension {:?} trying to determain file type", ext)),
     }
 }
@@ -19,10 +21,14 @@ impl SourceMediaInterface for GenericSingleFileItem {
     fn list_thumbnail(&self, _source_media_location: &PathBuf,  source_media_card: &PathBuf, _known_missing_files: Vec<PathBuf> ) -> Result<Vec<FileItem>> {
         filter_top_level_dir(source_media_card.as_path(),|_filename: &str, ext: Option<&str>, path: &str|{
             match ext {
-                Some("jpg") | Some("JPG") |
-                Some("mp4") | Some("MP4") |
-                Some("wav") | Some("WAV") => Ok(Some(create_simple_file(path.to_string(), filetype(ext.unwrap())?))),
-                Some(_) | None => Err(anyhow::anyhow!("Unexpected file {}", path)),
+                Some(a) => {
+                    match a.to_lowercase().as_str() {
+                        "jpg" | "png" | "mp4" | "wav" | "3gpp"
+                          => Ok(Some(create_simple_file(path.to_string(), filetype(ext.unwrap())?))),
+                        _ => Err(anyhow::anyhow!("File has no extension {}", path)),
+                    }
+                }
+                None => Err(anyhow::anyhow!("File has no extension {}", path)),
             }
         })
     }
