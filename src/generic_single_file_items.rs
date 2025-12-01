6117d3070a8e4cff0d2178a2d83ad36b154a886a
(Tim) Efthimis Kritikos <git-27@mail.efthimiskritikos.com>
2025-12-01 17:00:29 +0000

    Generic-Single-File-Items: Code clean-up and improved error handling

--- src/generic_single_file_items.rs
+++ src/generic_single_file_items.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result};
+use anyhow::{Result, anyhow};
 use crate::SourceMediaInterface;
 use std::path::{PathBuf};
 use crate::helpers::*;
@@ -8,48 +8,39 @@ use crate::helpers::FileType::*;
 
 pub struct GenericSingleFileItem;
 
-fn filetype(ext: &str) -> Result<crate::helpers::JsonFileInfoTypes> {
+fn filetype(ext: &str) -> Result<JsonFileInfoTypes> {
     match ext.to_lowercase().as_str() {
-        "jpg"   => Ok(JsonFileInfoTypes{ file_type:FileImage,item_type:ItemImage }),
-        "png"   => Ok(JsonFileInfoTypes{ file_type:FileImage,item_type:ItemImage }),
-        "mp4"   => Ok(JsonFileInfoTypes{ file_type:FileVideo,item_type:ItemVideo }),
-        "wav"   => Ok(JsonFileInfoTypes{ file_type:FileAudio,item_type:ItemAudio }),
-        "3gpp"  => Ok(JsonFileInfoTypes{ file_type:FileAudio,item_type:ItemAudio }),
-        _ => Err(anyhow::anyhow!("unkown file extension {:?} trying to determain file type", ext)),
+        "jpg"  => Ok(JsonFileInfoTypes{ file_type:FileImage, item_type:ItemImage }),
+        "png"  => Ok(JsonFileInfoTypes{ file_type:FileImage, item_type:ItemImage }),
+        "mp4"  => Ok(JsonFileInfoTypes{ file_type:FileVideo, item_type:ItemVideo }),
+        "wav"  => Ok(JsonFileInfoTypes{ file_type:FileAudio, item_type:ItemAudio }),
+        "3gpp" => Ok(JsonFileInfoTypes{ file_type:FileAudio, item_type:ItemAudio }),
+        _ => Err(anyhow!("unknown file extension {:?} trying to determain file type", ext)),
     }
 }
 
 impl SourceMediaInterface for GenericSingleFileItem {
     fn list_thumbnail(&self, _source_media_location: &PathBuf,  source_media_card: &PathBuf, _known_missing_files: Vec<PathBuf> ) -> Result<Vec<FileItem>> {
-        filter_dir(source_media_card.as_path(),|_filename: &str, ext: Option<&str>, _path: &PathBuf, path_str: &str|{
-            match ext {
-                Some(a) => {
-                    match a.to_lowercase().as_str() {
-                        "jpg" | "png" | "mp4" | "wav" | "3gpp"
-                          => {
-                              let types=filetype(ext.unwrap())?;
-                              match types.file_type{
-                                FileVideo | FileAudio => Ok(Some(create_part_file(path_str.to_string(), types,1,1,None))),
-                                FileImage => Ok(Some(create_simple_file(path_str.to_string(), types)?)),
-                                _ => Err(anyhow::anyhow!("unexpected file type")),
-                              }
-                          }
-                        _ => Err(anyhow::anyhow!("Unrecognised extension {}", path_str)),
-                    }
-                }
-                None => Err(anyhow::anyhow!("File has no extension {}", path_str)),
+        filter_dir(source_media_card.as_path(),|_filename: &str, input_ext: Option<&str>, _path: &PathBuf, path_str: &str|{
+            let ext = input_ext.ok_or_else(|| anyhow!("Expected filter_dir to provide a file extension"))?;
+            let types = filetype(ext)?;
+            match types.file_type{
+                FileVideo | FileAudio => Ok(Some(create_part_file(path_str.to_string(), types, 1, 1, None))),
+                FileImage => Ok(Some(create_simple_file(path_str.to_string(), types)?)),
+                _ => Err(anyhow!("Unrecognised extension '{}' in file '{}'", ext, path_str)),
             }
         })
     }
     fn list_high_quality(&self,  source_media_location: &PathBuf,  source_media_card: &PathBuf, known_missing_files: Vec<PathBuf> ) -> Result<Vec<FileItem>> {
-        self.list_thumbnail(source_media_location,source_media_card,known_missing_files)
+        self.list_thumbnail(source_media_location, source_media_card, known_missing_files)
     }
     fn get_related(&self, _source_media_location: &PathBuf, source_media_file: &PathBuf, _known_missing_files: Vec<PathBuf>) -> Result<Vec<FileItem>>{
-        let types=filetype(source_media_file.extension().unwrap().to_string_lossy().as_ref())?;
+        let extension = get_extension_str(source_media_file)?;
+        let types = filetype(extension)?;
         match types.file_type{
-            FileVideo => Ok(vec![create_part_file(source_media_file.to_string_lossy().into_owned(), types,1,1,None)]),
+            FileVideo => Ok(vec![create_part_file(source_media_file.to_string_lossy().into_owned(), types, 1, 1, None)]),
             FileImage => Ok(vec![create_simple_file(source_media_file.to_string_lossy().into_owned(), types)?]),
-            _ => Err(anyhow::anyhow!("unexpected file type")),
+            _ => Err(anyhow!("unexpected file type")),
         }
     }
     fn name(&self) -> String {
